5058d889f1e4887d07830ee3ae0c9976ea31c82d
brazlucca <brazlucca@gmail.com>
2022-12-22 19:36:14 -0300

    Primeiro commit

---
+++ src/main/java/com/springboot/VendasApplication.java
@@ -0,0 +1,12 @@
+package com.springboot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
