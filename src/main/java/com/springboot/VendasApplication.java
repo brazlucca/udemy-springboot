ecc9fee25e48edbfeb96330c80a8e4f1b2a9dd38
brazlucca <brazlucca@gmail.com>
2022-12-23 00:48:17 -0300

    Limpeza da aplicação e mapeamento h2

--- src/main/java/com/springboot/VendasApplication.java
+++ src/main/java/com/springboot/VendasApplication.java
@@ -1,45 +1,15 @@
 package com.springboot;
 
-import com.springboot.annotation.Cachorro;
-import com.springboot.annotation.Gato;
-import com.springboot.model.Animal;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
 
-    @Gato
-    public VendasApplication(Animal animal){
-        this.animal = animal;
-    }
-
-    private Animal animal;
-
-    @Bean
-    public CommandLineRunner executar(){
-        return args -> {
-          this.animal.fazerBarulho();
-        };
-    }
-
-    @Value("${applicationName}")
-    private String applicationName;
-
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
-
-    @GetMapping("/hello")
-    public String helloWorld() {
-        return applicationName;
-    }
-
 
 }
--- src/main/java/com/springboot/model/Animal.java
+++
@@ -1,6 +0,0 @@
-package com.springboot.model;
-
-public interface Animal {
-
-    void fazerBarulho();
-}
